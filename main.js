fd008ac638463377045dba2a010d19bd3f2b91e4
shrtshf <63200108+shrtshf@users.noreply.github.com>
2020-10-04 19:47:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var mobileMenu=document.getElementById("mobileMenuBar");
 var closeButton=document.getElementById("closeSideMenu");
 
 burgerButton.onclick=function() {
+
     mobileMenu.style.width="60vw";
+   
 }
 closeButton.onclick=function() {
     mobileMenu.style.width="0vw";
