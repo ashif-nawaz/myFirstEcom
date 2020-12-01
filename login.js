6e5fdf29b04258758437b94335f8b13b3a2590e1
shrtshf <63200108+shrtshf@users.noreply.github.com>
2020-12-01 18:13:35 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -6,25 +6,26 @@ function continueButtonClicked(){
     let continueButton = document.querySelector("#continueLogin");
     continueButton.addEventListener("click", function(e){
          e.preventDefault();
-         mobileNumberValidation();
+         let numberInput = document.getElementById("mobileNumber");
+             alert(numberInput.value);
     });
 }
 
 function mobileNumberValidation(){
-       let numberInput = document.getElementById("mobileNumber");
-            numberInput.style.background = "red";
-        // let warningTxt = document.querySelector(".warningMessage");
-        let warningTxt = numberInput.parentNode.querySelector(".warningMessage");
-        let valueString = "";
-            valueString = numberInput.value;
+    //    let numberInput = document.getElementById("mobileNumber");
+        //     numberInput.style.background = "red";
+        // // let warningTxt = document.querySelector(".warningMessage");
+        // let warningTxt = numberInput.parentNode.querySelector(".warningMessage");
+        // let valueString = "";
+        //     valueString = numberInput.value;
            
-        let numberPattern = /^([0-9]){10}$/;
-        if(!valueString.match(numberPattern)){
-            numberInput.style.borderColor = "red";
-            warningTxt.classList.add("active");
-        }else{
-            numberInput.style.borderColor = "gray";
-            warningTxt.classList.remove("active");
-        } 
+        // let numberPattern = /^([0-9]){10}$/;
+        // if(!valueString.match(numberPattern)){
+        //     numberInput.style.borderColor = "red";
+        //     warningTxt.classList.add("active");
+        // }else{
+        //     numberInput.style.borderColor = "gray";
+        //     warningTxt.classList.remove("active");
+        // } 
 
 }
