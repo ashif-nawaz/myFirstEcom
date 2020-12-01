4afa00fe93befd7f3d0b9ddefc5e075f32bb1eb3
shrtshf <63200108+shrtshf@users.noreply.github.com>
2020-12-01 18:56:46 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -10,26 +10,20 @@ function continueButtonClicked(){
     continueButton.addEventListener("click", function(e){
          e.preventDefault();
         //  let numberInput = document.getElementById("mobileNumber");
-             alert(numberInput);
-             warningTxt.classList.add("active");
+             mobileNumberValidation();
     });
 }
 
-// function mobileNumberValidation(){
-    //    let numberInput = document.getElementById("mobileNumber");
-        //     numberInput.style.background = "red";
-        // // let warningTxt = document.querySelector(".warningMessage");
-        // let warningTxt = numberInput.parentNode.querySelector(".warningMessage");
-        // let valueString = "";
-        //     valueString = numberInput.value;
-           
-        // let numberPattern = /^([0-9]){10}$/;
-        // if(!valueString.match(numberPattern)){
-        //     numberInput.style.borderColor = "red";
-        //     warningTxt.classList.add("active");
-        // }else{
-        //     numberInput.style.borderColor = "gray";
-        //     warningTxt.classList.remove("active");
-        // } 
+function mobileNumberValidation(){
+        let valueString = "";
+            valueString = numberInput.value;
+        let numberPattern = /^([0-9]){10}$/;
+        if(!valueString.match(numberPattern)){
+            numberInput.style.borderColor = "red";
+            warningTxt.classList.add("active");
+        }else{
+            numberInput.style.borderColor = "gray";
+            warningTxt.classList.remove("active");
+        } 
 
-// }
+}
